8f82f8d9cded8f225b3dfcf6517bfd41074ce6c6
fardinariful <fardinewucse2020@gmail.com>
2024-02-27 20:47:40 +0600

    js function added

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,49 @@ const ticketClass = "Economy";
 
 
 const allbtn=document.getElementsByClassName("add-btn");
-console.log(allbtn);
+
 
 
 for(const btn of allbtn){
     btn.addEventListener("click",function(event){
-        console.log(event.target.innerText);
+        const seatname=event.target.innerText;
+
+
+        const add_info=document.getElementById("show-info");
+        const div= document.createElement("div");
+        div.classList.add("flex","justify-between","w-full");
+
+        const p1=document.createElement("div");
+        const p2=document.createElement("div"); 
+        const p3=document.createElement("div");
+
+        p1.innerText=seatname;
+        p2.innerText=ticketClass;
+        p3.innerText=seatPrice;
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+  
+        add_info.appendChild(div);
+        
+        updatedtotalcost(seatPrice);
+        
     })
+ 
+    
 }
+
+function updatedtotalcost(value){
+    
+    const total_cost=document.getElementById("total-price");
+    const sum=parseInt(total_cost.innerText)+value;
+    total_cost.innerText=sum;
+    
+    
+}
+
+
+
 
 
